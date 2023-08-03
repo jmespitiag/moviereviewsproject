9775024cc51fa6d1c63da8840405a81f6f1a73be
jmespitiag <jmespitiag@eafit.edu.co>
2023-08-03 11:31:04 -0500

    Archivo html

--- movie/views.py
+++ movie/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Welcome to Home page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html',{'name':'Martín Espitia'})
+    return render(request, 'home.html',{'name':'Martin Espitia'})
 
 # Create your views here.
