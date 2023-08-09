60a50e1bdd8b8ef86c74f51ea04734e417a56833
jmespitiag <jmespitiag@eafit.edu.co>
2023-08-09 17:03:06 -0500

    Visualización de Peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home page</h1>')
     #return render(request, 'home.html')
-    return render(request, 'home.html',{'name':'Martin Espitia'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(tittle__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+
+     
+    return render(request, 'home.html',{'searchTeam':searchTerm, 'movies':movies})
+
 
 def about(request):
     return render(request,'about.html')
